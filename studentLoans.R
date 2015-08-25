86c6bf879a5536bef5fd220007362e7bd4ae2199
eamoncaddigan <eamon.caddigan@gmail.com>
2015-08-25 13:47:03 -0400

    Defining dumb constants.

---
+++ studentLoans.R
@@ -0,0 +1,9 @@
+# Gonna see how many hours I'd have to work at minimum wage each week to afford
+# my student load payments.
+
+# This is really personal
+monthlyPayment <- 524.75
+
+# A lazy coder would just use '4'. Not this guy.
+weeksPerMonth <- 365.252 / 12 / 7 # days/year * year/month * weeks/day
+
