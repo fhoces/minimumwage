f97350981177c1a3f0d7b825794aef1708578c4f
eamoncaddigan <eamon.caddigan@gmail.com>
2015-08-25 16:11:59 -0400

    All set (I think).

--- studentLoans.R
+++ studentLoans.R
@@ -7,11 +7,27 @@ library("maptools")
 library("ggplot2")
 library("dplyr")
 
-# This is really personal
+
+# Money stuff -------------------------------------------------------------
+
+# This is really personal!
 monthlyPayment <- 524.75          # dollars/month
 
 # A lazy coder would just use '4'. Not this guy.
 weeksPerMonth <- 365.252 / 12 / 7 # days/year * year/month * weeks/day
+
+# Read in state minimum wages.
+minimumWage <- read.csv("minimum_wage.csv", stringsAsFactors = FALSE)
+# Yes, GA and WY have state minimum wages set below the federal level.
+# Also discretize the hours/week for pretty colorbrewer palettes. Should be <= 9
+minimumWage <- minimumWage %>%
+  mutate(real_minimum = pmax(federal_minimum, state_minimum, 
+                             na.rm = TRUE),
+         hours = monthlyPayment / weeksPerMonth / real_minimum,
+         hours_cut = cut(hours, 6))
+
+
+# Handle the map ----------------------------------------------------------
 
 # Load the map
 stateMap <- readOGR("times-approximate/shp", "Admin1_Polygons")
@@ -22,14 +38,24 @@ stateMap.data <- select(stateMap@data, id, ISO3166_2)
 stateMapProj <- spTransform(stateMap, CRS("+proj=merc"))
 # Create data.frame and join in state codes
 stateMapDF <- fortify(stateMapProj, region = "id")
-stateMapDF <- left_join(stateMapDF, stateMap.data, by="id")
+stateMapDF <- left_join(stateMapDF, stateMap.data, by = "id")
+
+# Combine the money stuff with the map
+stateMapDF <- left_join(stateMapDF, minimumWage, by = "ISO3166_2")
+
+
+# Plotting ----------------------------------------------------------------
 
 # Draw the map
-# TODO: actual fill data
-p1 <- ggplot(stateMapDF, aes(long, lat, group=group, fill=ISO3166_2)) + 
+p1 <- ggplot(stateMapDF, aes(long, lat, group=group, fill=hours_cut)) + 
   geom_polygon() + 
-  geom_path(color="black", size=0.3) + 
+  geom_path(color="black", size=1) + 
+  scale_fill_brewer(type = "seq", palette = "YlGn",
+                    name = "Hours",
+                    labels = sub("\\(([0-9\\.]+),([0-9\\.]+)\\]", "\\1 - \\2", 
+                                 levels(stateMapDF$hours_cut))) + 
   coord_equal() + 
+  ggtitle("Hours of work by state") + 
   list(theme(panel.grid.minor = element_blank(),
              panel.grid.major = element_blank(),
              panel.background = element_blank(),
@@ -40,5 +66,7 @@ p1 <- ggplot(stateMapDF, aes(long, lat, group=group, fill=ISO3166_2)) +
              axis.text.y = element_blank(),
              axis.ticks = element_blank(),
              axis.title.x = element_blank(),
-             axis.title.y = element_blank()))
+             axis.title.y = element_blank(),
+             plot.title = element_text(size=32)))
 print(p1)
+ggsave("hours_per_state.png", plot = p1)
