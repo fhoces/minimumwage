7d090e641a4a554cd14a4d942fc74d9f69eba7cb
eamoncaddigan <eamon.caddigan@gmail.com>
2015-08-26 22:21:22 -0400

    Scraping wages.

--- scrapeLivingWage.R
+++ scrapeLivingWage.R
@@ -9,32 +9,47 @@ library(tidyr)
 livingWageList <- list()
 for (stateIdx in 1:56) {
   # Try to get the state's source
-  url <- sprintf("http://livingwage.mit.edu/states/%0.2d", stateIdx)
-  stateHTML <- url %>%
-    html()
-  
-  # Scrape out the (long) name of the state
-  stateName <- stateHTML %>% 
-    html_nodes(xpath="/html/body/div[1]/div[2]/h1") %>% 
-    html_text()
-  stateName <- sub("Living Wage Calculation for ([[:alpha:]]+).*", "\\1", stateName)
-  
-  # Grab the short living wage table
-  stateLivingWage <- stateHTML %>%
-    html_nodes(xpath="/html/body/div[1]/div[2]/div[1]/table") %>%
-    html_table()
-  stateLivingWage <- stateLivingWage[[1]]
-  
-  # Add the state name to the table and tidy the data
-  livingWageList[[length(livingWageList)+1]] <- stateLivingWage %>% 
-    gather("household_type", "hourly_wage", -1) %>% 
-    rename(wage_level = `Hourly Wages`) %>% 
-    mutate(state_name = stateName, 
-           hourly_wage = as.numeric(sub("^\\$", "", hourly_wage)))
+  stateHTML <- tryCatch(html(sprintf("http://livingwage.mit.edu/states/%0.2d", stateIdx)),
+                        error = function(cond) { return(NULL) },
+                        warning = function(cond) { return(NULL) },
+                        finally = NA)
+  if (!is.null(stateHTML)) {
+    # Scrape out the (long) name of the state
+    stateName <- stateHTML %>% 
+      html_nodes(xpath="/html/body/div[1]/div[2]/h1") %>% 
+      html_text()
+    stateName <- sub("Living Wage Calculation for (.*)", "\\1", stateName)
+
+    
+    # Grab the short living wage table
+    stateLivingWage <- stateHTML %>%
+      html_nodes(xpath="/html/body/div[1]/div[2]/div[1]/table") %>%
+      html_table()
+    stateLivingWage <- stateLivingWage[[1]]
+    
+    # Add the state name to the table and tidy the data
+    livingWageList[[length(livingWageList)+1]] <- stateLivingWage %>% 
+      gather("household_type", "hourly_wage", -1) %>%
+      rename(wage_level = `Hourly Wages`) %>% 
+      mutate(state_name = stateName, 
+             hourly_wage = as.numeric(sub("^\\$", "", hourly_wage)))
+  }
 }
-livingWageDF <- bind_rows(livingWageList)
+livingWage <- bind_rows(livingWageList)
 
 
-# Pull out the redundant minimum wage info --------------------------------
+# Pull out the redundant minimum wage info into its own table -------------
+
+minimumWage <- livingWage %>% 
+  filter(wage_level == "Minimum Wage") %>% 
+  select(state_name, hourly_wage) %>% 
+  distinct()
+
+livingWage <- livingWage %>%
+  filter(wage_level != "Minimum Wage")
 
 
+# Save DFs to CSVs --------------------------------------------------------
+
+write.csv(minimumWage, "minimum_wage.csv", row.names = FALSE)
+write.csv(livingWage, "living_wage.csv", row.names = FALSE)
